620a15fc53baf8365b93a5a34fafcc64382d813f
Kirk Shelton <sheltk68@gmail.com>
2015-04-20 12:30:45 -0400

    Changes made to custom.css file

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "imgres.png",:convert_options => {:medium => '-background white -gravity center -extent 200x200'}
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "imgres.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
